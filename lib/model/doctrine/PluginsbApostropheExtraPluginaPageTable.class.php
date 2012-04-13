8bad6953e9348659fe7d8c54ecb606acba7ed26d
Giles Smith <giles@superrb.com>
2012-04-13 12:22:38 +0100

    Added support for Apostrophe fast-hydrate

--- lib/model/doctrine/PluginsbApostropheExtraPluginaPageTable.class.php
+++ lib/model/doctrine/PluginsbApostropheExtraPluginaPageTable.class.php
@@ -9,10 +9,11 @@ class PluginsbApostropheExtraPluginaPageTable extends PluginaPageTable
 {
   public static function listProjectPageCategories()
   {
+    $fast = sfConfig::get('app_a_fasthydrate', false);
     return Doctrine::getTable('aCategory')->createQuery('c')
             ->innerJoin('c.ProjectPages AS p')
             ->orderBy('c.name')
             ->groupBy('c.id')
-            ->execute();
+            ->execute(array(), $fast ? Doctrine::HYDRATE_ARRAY : Doctrine::HYDRATE_RECORD);
   }
 }
